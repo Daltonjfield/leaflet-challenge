4dbde4dd83bd7bbce9a5e7dc95887dbc73149715
Dalton Field <daltonfield@Daltons-MacBook-Pro.local>
2021-01-21 19:22:23 -0700

    adjusting the size and colors of the circles, still need to add legend

--- Leftlet-Step-1/static/js/logic.js
+++ Leftlet-Step-1/static/js/logic.js
@@ -6,20 +6,20 @@
     if (depth > 90)
     return "red";
     else if (depth > 70)
-    return "orange";
+    return "pink";
     else if (depth > 50)
-    return "light orange";
+    return "orange";
     else if (depth > 30)
     return "yellow";
     else if (depth >10)
-    return "light green";
+    return "lime";
     else
     return "green";
   }
   
   // Function that will determine the size of marker based on the magnitute of the earthquake
     function chooseSize(mag) {
-        return (mag*3000)
+        return (mag*30000)
 
     }
 
@@ -35,7 +35,7 @@
             
             // Called on each feature
             onEachFeature: function(feature, layer) {
-                layer.bindPopup("<h1>" + feature.properties.place + "</h1> <hr> <h2>" + feature.properties.magnitude + "</h2>" + feature.geometry.coordinates[2])
+                layer.bindPopup("<h1>" + feature.properties.place + "</h1> <hr> Magnitude <h2>" + feature.properties.mag + "</h1> <hr> Depth <h2>" + feature.geometry.coordinates[2])
             },
             pointToLayer: function (feature, latlong){
                 return new L.circle(latlong, {
@@ -56,8 +56,8 @@
     function createMap(earthquakedata){
         // Creating map object
     var myMap = L.map("mapid", {
-    center: [40.7128, -74.0059],
-    zoom: 3
+    center: [40.7128, -94.0059],
+    zoom: 5
     });
   
   // Adding tile layer
