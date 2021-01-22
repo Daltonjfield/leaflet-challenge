0035fed61ea6e766cb3cbdf4f81c6ce4aa4e7c51
Dalton Field <daltonfield@Daltons-MacBook-Pro.local>
2021-01-21 18:50:24 -0700

    adding the buttons onto the mapand addint the tile layer

--- Leftlet-Step-1/static/js/logic.js
+++ Leftlet-Step-1/static/js/logic.js
@@ -1,19 +1,3 @@
-// Creating map object
-var myMap = L.map("mapid", {
-    center: [40.7128, -74.0059],
-    zoom: 3
-  });
-  
-  // Adding tile layer
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
-  
   // Use this link to get the usgsjson data.
   var usgs = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
   
@@ -68,4 +52,45 @@ var myMap = L.map("mapid", {
         //calling createMap function
         createMap(earthquakedata)
     }
+    //create the createMap function
+    function createMap(earthquakedata){
+        // Creating map object
+    var myMap = L.map("mapid", {
+    center: [40.7128, -74.0059],
+    zoom: 3
+    });
   
+  // Adding tile layer
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    }).addTo(myMap);
+
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+      });
+    
+      // Define a baseMaps object to hold our base layers
+      var baseMaps = {
+        "Street Map": streetmap,
+        "Dark Map": darkmap
+      };
+    
+      // Create overlay object to hold our overlay layer
+      var overlayMaps = {
+        Earthquakes: earthquakedata
+      };
+      // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+    }
